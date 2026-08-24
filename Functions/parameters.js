df2d2b3f5bb82039c28569b74b587c6ecc2f4f76
OmRana2004 <omrana8755800322@gmail.com>
2026-08-25 01:13:17 +0530

    parameter-argument

--- Functions/parameters.js
+++ Functions/parameters.js
@@ -9,23 +9,47 @@
 // }
 // console.log(hero(4))
 
-function result(marks) {
-    const aGrad = () => {
-      return "EXCELENT"
-    }
-    const bGrad = () => {
-        return "GOOD"
-    }
-    const cGrad = () => {
-        return "FAIR"
-    }
+// function result(marks) {
+//     const aGrad = () => {
+//       return "EXCELENT"
+//     }
+//     const bGrad = () => {
+//         return "GOOD"
+//     }
+//     const cGrad = () => {
+//         return "FAIR"
+//     }
 
-    if (marks>80) {
-        return aGrad
-    }else if (marks>60) {
-        return bGrad
-    } else {
-        return cGrad
-    } 
-} 
-console.log(result(88)())
+//     if (marks>80) {
+//         return aGrad
+//     }else if (marks>60) {
+//         return bGrad
+//     } else {
+//         return cGrad
+//     } 
+// } 
+// console.log(result(88)())
+
+
+// const greet = (name, age) => {
+//     console.log(name, age)
+// }
+
+// greet("om", 22)
+
+function calculator(a, b) {
+    
+    const add = () => (a+b)
+    
+
+    const sub = () => (a-b)
+   
+
+    const mult = () => (a*b)
+    
+
+    const div = () => (a/b)
+       return[add(), sub(), mult(), div()]
+}
+
+console.log(calculator(12,2))
