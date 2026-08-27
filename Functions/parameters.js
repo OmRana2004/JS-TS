690f8f2fa939ebb8a0a68c49779e7ae5111fc258
OmRana2004 <omrana8755800322@gmail.com>
2026-08-28 01:27:52 +0530

    hof-calback-prac

--- Functions/parameters.js
+++ Functions/parameters.js
@@ -26,10 +26,9 @@
 //         return bGrad
 //     } else {
 //         return cGrad
-//     } 
-// } 
+//     }
+// }
 // console.log(result(88)())
-
 
 // const greet = (name, age) => {
 //     console.log(name, age)
@@ -38,15 +37,12 @@
 // greet("om", 22)
 
 // function calculator(a, b) {
-    
+
 //     const add = () => (a+b)
-    
 
 //     const sub = () => (a-b)
-   
 
 //     const mult = () => (a*b)
-    
 
 //     const div = () => (a/b)
 //        return[add(), sub(), mult(), div()]
@@ -54,3 +50,33 @@
 
 // console.log(calculator(12,2))
 
+//  1 CODE
+
+function orderFood(hof) {
+  console.log("Food is Preparing");
+
+  setTimeout(() => {
+    hof();
+  }, 3000);
+}
+
+function orderReady() {
+  console.log("Your order is ready sir!");
+}
+
+orderFood(orderReady);
+
+// // 2 CODE
+
+// function orderFood() {
+//   console.log("Your Food is preparing");
+// }
+
+// const orderReady = () => {
+//   setTimeout(() => {
+//     console.log("Your order is redy sir!");
+//   }, 3000);
+// };
+
+// orderFood();
+// orderReady();
