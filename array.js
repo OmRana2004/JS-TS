b5804cf60aac0eaa61eead13394b49aa3ce56fb4
OmRana2004 <omrana8755800322@gmail.com>
2026-08-29 23:50:27 +0530

    ,length-1 concept

--- array.js
+++ array.js
@@ -1,8 +1,17 @@
-const pri = ["coding", "gaming", "sleeping", "eating", "socializing"];
-const newPri = pri.filter((charS) => charS.startsWith('s') )
-console.log(newPri);
- console.log(pri[4])
+// const pri = ["coding", "gaming", "sleeping", "eating", "socializing"];
+// const newPri = pri.filter((charS) => charS.startsWith('s') )
+// console.log(newPri);
+//  console.log(pri[4])
 
+// setTimeout(() => {
+//    pri [4] = "SOCILA", console.log(pri[4])
+// },2000)
+
+
+// FINDING THE INDEX OF THE ARRAY
+const name = ["RAN","SITA","LAXMAN","HANUMAN"];
+
+console.log(name.length)
 setTimeout(() => {
-   pri [4] = "SOCILA", console.log(pri[4])
+    console.log(name[name.length-1])
 },2000)
