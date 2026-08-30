56683bf229757d1b4053f8ebfeb419cedfaeb407
OmRana2004 <omrana8755800322@gmail.com>
2026-08-30 22:58:24 +0530

    array

--- Array/array.js
+++ Array/array.js
@@ -9,9 +9,25 @@
 
 
 // FINDING THE INDEX OF THE ARRAY
-const name = ["RAN","SITA","LAXMAN","HANUMAN"];
+const name = ["RAM","SITA","LAXMAN","HANUMAN"];
+name.pop()
+name.push("hanuman")
+name.shift()
+name.unshift("RAM")
+name.reverse()
 
-console.log(name.length)
-setTimeout(() => {
-    console.log(name[name.length-1])
-},2000)
+// console.log(name.length)
+// setTimeout(() => {
+//     console.log(name[name.length-1])
+// },2000)
+
+// console.log(name)
+
+const arr = []
+
+for(let i = 1; i<=10; i++)
+    if(i % 2 === 0) {
+        arr.push(i)
+    }
+
+console.log(arr)
