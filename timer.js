975802e7d542ee0357ba80d86aa5665f0856e21c
OmRana2004 <omrana8755800322@gmail.com>
2026-08-30 11:00:59 +0530

    difference-b/t-setTimeout-setInterval-with-the concept-of id

--- timer.js
+++ timer.js
@@ -3,3 +3,12 @@
 // }
 // setTimeout(Timer, 2000)
 
+let c =1
+const chant = setInterval(() => {
+    console.log(c++,"SITA RAM")
+},2000)
+
+setTimeout(() => {
+    clearInterval(chant)
+    console.log("Jai Shri Ram")
+},20000)
