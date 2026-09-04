39db8f446a64b33df65a8baf62936c3caecb7d41
OmRana2004 <omrana8755800322@gmail.com>
2026-09-05 01:25:31 +0530

    rest&spread-operator

--- Arrays/array.js
+++ Arrays/array.js
@@ -120,9 +120,13 @@ const [a,b,c,d] = arr
 console.log(a,b,c,d)
 */
 
+
+        //rest operator
+const [first, ...rest] = [10,20,30,40]
+console.log(first,rest)
+
+        //spread operator
 const arr = [10,20,30,40]
-const arr1 = arr;
-
-arr1.push(60)
-
+const arr1 = [...arr]
+    arr1.push(60)
 console.log(arr,arr1)
