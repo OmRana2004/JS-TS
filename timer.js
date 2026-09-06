90746e9f89334f9e99b1efc6e282e97baee784a9
OmRana2004 <omrana8755800322@gmail.com>
2026-09-06 23:31:16 +0530

    ..

--- timer.js
+++ timer.js
@@ -3,12 +3,18 @@
 // }
 // setTimeout(Timer, 2000)
 
-let c =1
-const chant = setInterval(() => {
-    console.log(c++,"SITA RAM")
-},2000)
+// let c =1
+// const chant = setInterval(() => {
+//     console.log(c++,"SITA RAM")
+// },2000)
 
-setTimeout(() => {
-    clearInterval(chant)
-    console.log("Jai Shri Ram")
-},20000)
+// setTimeout(() => {
+//     clearInterval(chant)
+//     console.log("Jai Shri Ram")
+// },20000)
+
+let a = "6";
+let b = 7;
+let c = a-b
+
+console.log(c)
