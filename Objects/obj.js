4729239078a1f9c8c52edcdc48f635b19d36f0de
OmRana2004 <omrana8755800322@gmail.com>
2026-09-10 23:01:04 +0530

    objects

---
+++ Objects/obj.js
@@ -0,0 +1,18 @@
+                                    //OBJECTS LITERALS
+
+let user = {
+    name: "om",
+    "full Name": "om rana",
+    age: 22,
+    location: "Uttarkashi",
+    email: "ranaom2025@gamil.com"
+}
+
+const key = "age";
+
+        // DOT NOTATION
+console.log(user.email)
+        // BRACKER NOATATION
+console.log(user["full Name"])
+        // DYNAMIC PROPERTY
+console.log(user[key])
