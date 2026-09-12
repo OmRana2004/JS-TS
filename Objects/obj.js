4073a7e1d674555bbf0ba4204138231c2bca9ca7
OmRana2004 <omrana8755800322@gmail.com>
2026-09-12 11:40:21 +0530

    objects

--- Objects/obj.js
+++ Objects/obj.js
@@ -1,18 +1,10 @@
                                     //OBJECTS LITERALS
 
-let user = {
+const user = {
     name: "om",
-    "full Name": "om rana",
-    age: 22,
-    location: "Uttarkashi",
-    email: "ranaom2025@gamil.com"
+    lName: "rana",
+    city: "saura",
+    address: "saura"
 }
 
-const key = "age";
-
-        // DOT NOTATION
-console.log(user.email)
-        // BRACKER NOATATION
-console.log(user["full Name"])
-        // DYNAMIC PROPERTY
-console.log(user[key])
+console.log(user.lName)
--- timer.js
+++ timer.js
@@ -13,11 +13,13 @@
 //     console.log("Jai Shri Ram")
 // },20000)
 
-const s = 10
-const s1 = 20
-const s2 = 30
-const s3 = 40
+// const s = 10
+// const s1 = 20
+// const s2 = 30
+// const s3 = 40
 
-console.log(Array.isArray("om"))
-console.log(Array.from("om"))
-console.log(Array.of(s,s1,s2,s3))
+// console.log(Array.isArray("om"))
+// console.log(Array.from("om"))
+// console.log(Array.of(s,s1,s2,s3))
+arr = [1,2,3,4,5,6]
+console.log(typeof(arr))
