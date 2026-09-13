0e7e298c7fecf05e30e7d9d151cbe801345f2ead
OmRana2004 <omrana8755800322@gmail.com>
2026-09-14 01:30:46 +0530

    reacap

--- timer.js
+++ timer.js
@@ -1,25 +1,10 @@
-// function Timer() {
-//     console.log("good")
-// }
-// setTimeout(Timer, 2000)
+const arr1 = ["OM"]
+const arr2 = ["RANA"]
 
-// let c =1
-// const chant = setInterval(() => {
-//     console.log(c++,"SITA RAM")
-// },2000)
+const n = arr1.concat(arr2)
+console.log(n);
+console.log(n.includes("OM"));
 
-// setTimeout(() => {
-//     clearInterval(chant)
-//     console.log("Jai Shri Ram")
-// },20000)
 
-// const s = 10
-// const s1 = 20
-// const s2 = 30
-// const s3 = 40
 
-// console.log(Array.isArray("om"))
-// console.log(Array.from("om"))
-// console.log(Array.of(s,s1,s2,s3))
-arr = [1,2,3,4,5,6]
-console.log(typeof(arr))
+
