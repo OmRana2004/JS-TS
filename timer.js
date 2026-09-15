e9fdad3d51880bd928e1bcb36735efd42af01adf
OmRana2004 <omrana8755800322@gmail.com>
2026-09-15 20:21:54 +0530

    ..

--- timer.js
+++ timer.js
@@ -1,8 +1,16 @@
-const arr = [1,2,3];
-    const newArr = arr.map(x=>x*2);
-    console.log(newArr);
-    
+// const orderFood = (hof) => {
+//     console.log("YOUR FOOD IS PREPARIG!");
 
+//     setTimeout(()=> {
+//         hof()
+//     },2000)
+// }
 
+// const ready = () => {
+//     console.log("YOUR FOOD IS READY SIR!")
+// }
+// orderFood(ready);
 
-
+arr = [1,1,1,3,15,41,53,21]
+newArr = arr.map(x=>x*2)
+console.log(newArr);
