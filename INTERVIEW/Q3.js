3de81a911dc50c7aaae8bdafa0ea2fe5acd6073e
OmRana2004 <omrana8755800322@gmail.com>
2026-09-20 12:02:32 +0530

    loop

--- INTERVIEW/Q3.js
+++ INTERVIEW/Q3.js
@@ -9,3 +9,4 @@
 
   console.log(reverse === str);
   
+  
--- timer.js
+++ timer.js
@@ -11,11 +11,12 @@
 // }
 // orderFood(ready);
 
-const isPalandorm = (str) => {
-    return str.split("").reverse("").join("");
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+  console.log(" ");
 }
-console.log(isPalandorm("OM RANA"));
-
 
 
 
