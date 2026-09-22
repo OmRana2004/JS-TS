2493b3df91204868678c463f47fcb6947a8bbb1c
OmRana2004 <omrana8755800322@gmail.com>
2026-09-22 22:30:31 +0530

    aeiou

---
+++ INTERVIEW/Q4.js
@@ -0,0 +1,13 @@
+  // COUNT HOW MANY VOWELS IN THE STRING
+
+function countVowels(str){
+    let count = 0
+    for(let i = 0; i < str.length; i++) {
+        if("aeiouAEIOU".includes(str[i])){
+            count++
+        }
+    }
+
+    return count;
+}
+console.log(countVowels("omrana"));
